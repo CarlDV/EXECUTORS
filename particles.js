6c5b8ee9b159b4aaa6eb4891f331a2b11dbea794
Daviddd <147702373+CarlDV@users.noreply.github.com>
2025-01-29 12:08:01 +0800

    Update particles.js

--- particles.js
+++ particles.js
@@ -1,16 +1,1 @@
-const particlesContainer = document.querySelector('.particles');
 
-function createParticle() {
-    const particle = document.createElement('div');
-    particle.classList.add('particle');
-    particle.style.left = `${Math.random() * 100}%`;
-    particle.style.top = `${Math.random() * 100}%`;
-    particle.style.animationDuration = `${Math.random() * 5 + 3}s`;
-    particlesContainer.appendChild(particle);
-
-    setTimeout(() => {
-        particle.remove();
-    }, 8000);
-}
-
-setInterval(createParticle, 300);
